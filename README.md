f378787a9339cd2ead46d315b1de383fe682c480
Daniele Orler <daniele.orler@fortum.com>
2020-05-31 16:01:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# semver-action
+
+Simple action to calculate next version of a project
+
+## Usage
+
+### calculate next version and create a release
+The following configuration will calculate the next version and create a release (tag head and create changelog since previous release)
+```
+- uses: fortum/semver-action
+  id: semver
+  with:
+    repo-token: ${{ secrets.GITHUB_TOKEN }}
+    release: true
+```
+
+### calculate next version without releasing
+The following configuration will calculate the next version without tagging
+```
+- uses: fortum/semver-action
+  id: semver
+  with:
+    repo-token: ${{ secrets.GITHUB_TOKEN }}
+    release: false
+```
+
+### retrieve calculated version
+```
+- uses: fortum/semver-action
+  id: semver
+  with:
+    repo-token: ${{ secrets.GITHUB_TOKEN }}
+    release: false
+
+- name: prints next version
+  run: echo ${{steps.semver.outputs.next-version}}
+```
+
+## notes
+When invoking semveraction on branch master the version will be pure semver (i.e. 1.2.0)   
+When invoking from aby other branch the version will be `branch_name-1.2.0`
---
+++ src/index.js
@@ -0,0 +1,98 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const semverRexEx = /^[0-9]+.[0-9]+.[0-9]+$/
+
+async function getLastRelease(client) {
+    const lastRelease = await client.repos.getLatestRelease({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+    });
+    const rawVersion = lastRelease.data.tag_name;
+
+    if (!(rawVersion && semverRexEx.test(rawVersion))) {
+        core.warning(`never versioned before or ${rawVersion} is not a valid semver tag`);
+        return "0.0.0";
+    }
+
+    return rawVersion;
+}
+
+async function buildChangeLog(client, lastTaggedCommitSha, nextVersion) {
+    const commitsSinceLastTag = await client.repos.listCommits({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        per_page: 100
+    });
+
+    const commits = commitsSinceLastTag.data;
+    let i = 0;
+    let changeLog = `# Version ${nextVersion}\n`;
+    changeLog += "changes:\n";
+    while (lastTaggedCommitSha !== commits[i].sha) {
+        changeLog += `* ${commits[i].commit.message}\n`;
+        i++;
+    }
+
+    return changeLog;
+}
+
+async function release(client, changeLog, nextVersion) {
+    await client.git.createRef({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        ref: `refs/tags/${nextVersion}`,
+        sha: github.context.sha
+    });
+    await client.repos.createRelease({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        tag_name: nextVersion,
+        name: `Version ${nextVersion}`,
+        body: changeLog
+    });
+}
+
+async function calculateNextVersion(rawVersion, rawBranch) {
+    const [branch] = rawBranch.split("/").slice(-1);
+    let [major, minor, path] = rawVersion.split(".").map(part => parseInt(part));
+    console.log("Previous version: " + rawVersion)
+    minor += 1;
+    const nextVersion =  [major, minor, path].join(".");
+    console.log("Next version: " + nextVersion);
+    return branch === "master" ? nextVersion : branch + "-" + nextVersion;
+}
+
+async function run() {
+    try {
+        const token = core.getInput('repo-token', {required: true});
+        const client = new github.GitHub(token);
+
+        const rawVersion = await getLastRelease(client);
+        const nextVersion = await calculateNextVersion(rawVersion, github.context.payload.ref);
+        core.setOutput("next-version", nextVersion);
+
+        if (core.getInput('release') === 'false') {
+            return;
+        }
+
+        if (rawVersion === "0.0.0") {
+            const changeLog = await buildChangeLog(client, null, nextVersion);
+            await release(client, changeLog, nextVersion);
+            return;
+        }
+
+        const lastTag = await client.git.getRef({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            ref: `tags/${rawVersion}`,
+        });
+
+        const changeLog = await buildChangeLog(client, lastTag.data.object.sha, nextVersion);
+        await release(client, changeLog, nextVersion);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();

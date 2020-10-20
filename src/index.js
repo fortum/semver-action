1e06b74a3d234b4990419e3b0df6ed5013e0ae35
Daniele Orler <daniele.orler@fortum.com>
2020-10-20 10:24:59 +0200

    customize release branch

--- src/index.js
+++ src/index.js
@@ -70,14 +70,14 @@ async function release(client, changeLog, nextVersion) {
     });
 }
 
-async function calculateNextVersion(rawVersion, rawBranch) {
+async function calculateNextVersion(rawVersion, rawBranch, releaseBranch) {
     const branch = rawBranch.replace(new RegExp("^refs/heads|\/", "g"), "");
     let [major, minor, path] = rawVersion.split(".").map(part => parseInt(part));
     console.log("Previous version: " + rawVersion)
     minor += 1;
     const nextVersion = [major, minor, path].join(".");
     console.log("Next version: " + nextVersion);
-    return branch === "master" ? nextVersion : branch + "-" + nextVersion;
+    return branch === releaseBranch ? nextVersion : branch + "-" + nextVersion;
 }
 
 async function run() {
@@ -85,9 +85,10 @@ async function run() {
         const token = core.getInput('repo-token', {required: true});
         const prefix = core.getInput('version-prefix');
         const client = new github.GitHub(token);
+        const releaseBranch = core.getInput('release-branch');
 
         const rawVersion = await getLastRelease(client, prefix);
-        const nextVersion = prefix + await calculateNextVersion(rawVersion, github.context.payload.ref, prefix);
+        const nextVersion = prefix + await calculateNextVersion(rawVersion, github.context.payload.ref, releaseBranch);
         console.log("prefixed next version: " + nextVersion);
         core.setOutput("next-version", nextVersion);
 
